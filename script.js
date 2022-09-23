fdc74705caa663cbc9b2d22ce8f99c9a3ef822b0
Paolo Bietolini <pbietolini@gmail.com>
2022-09-24 00:08:56 +0200

    'changes'

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const calcola = document.getElementById('calcola');
 const input = document.querySelectorAll(".valoriInput");
+
 let valoriInput = {};
 let risultati = new Array(3);
 
-
 for (let i = 0; i < risultati.length; i++) {
-  risultati[i] = {Kcal: 0, Grammi: 0, Percentuale: 0}
-  //risultati.push({});
-};
+  risultati[i] = {
+    Kcal: 0,
+    Grammi: 0,
+    Percentuale: 0
+  };
+}
+const table = document.querySelector("table");
+let data = Object.keys(risultati[0]);
 
-let totalePro, totaleFat, totaleCarb;
-let totaleGrPro, totaleGrFat, totaleGrCarb;
-
-function generateTableHead(table, data) {
+const generateTableHead = (table, data) => {
   let thead = table.createTHead();
   let row = thead.insertRow();
   for (let key of data) {
@@ -21,9 +23,8 @@ function generateTableHead(table, data) {
     th.appendChild(text);
     row.appendChild(th);
   }
-}
-
-function generateTable(table, data) {
+};
+const generateTable = (table, data) => {
   for (let element of data) {
     let row = table.insertRow();
     for (key in element) {
@@ -32,45 +33,32 @@ function generateTable(table, data) {
       cell.appendChild(text);
     }
   }
+  
+};
+
+const calcolo = () => {
+  let i = 0;
+  while (i < risultati.length) {
+    let n = i % 2 === 0 ? 4 : 9;
+    let qty = i === 0 ? valoriInput.qtyPro : valoriInput.qtyFat;
+    risultati[i].Kcal = i !== 2 ? Math.round(valoriInput.peso * qty * n * 1e2) / 1e2 : valoriInput.totaleKcal - (risultati[1].Kcal + risultati[0].Kcal);
+    risultati[i].Grammi = Math.round(risultati[i].Kcal / n * 1e2) / 1e2;
+    risultati[i].Percentuale = Math.round(risultati[i].Kcal / valoriInput.totaleKcal * 100) || 0;
+    i++;
+  };
+  valoriInput.qtyCarb = Math.round(risultati[2].Grammi / valoriInput.peso * 1e2) / 1e2;
+  document.getElementById('qtyCarb').value = Math.round(risultati[2].Grammi / valoriInput.peso * 1e2) / 1e2 || '';
 }
 
-let table = document.querySelector("table");
-let data = Object.keys(risultati[0]);
-
-let p = document.createElement('p');
-let div = document.createElement('div');
-
-
-
-calcola.addEventListener('click', function () {
-  if (input[0].value == '') {
-    p.innerText = 'Riempi il form';
-    p.classList.add('errore');
-    document.body.appendChild(p);
-  } else {
-    input.forEach(e => valoriInput[e.id] = Math.round(e.value * 1e2) / 1e2);
-
-    let i = 0;
-    while (i < risultati.length) {
-      let n = i % 2 === 0 ? 4 : 9;
-      let qty = i === 0 ? valoriInput.qtyPro : valoriInput.qtyFat;
-      risultati[i]['Kcal'] = i !== 2 ? Math.round(valoriInput.peso * qty * n * 1e2) / 1e2 : valoriInput.totaleKcal - (risultati[1]['Kcal'] + risultati[0]['Kcal']);
-      risultati[i]['Grammi'] = Math.round(risultati[i]['Kcal'] / n * 1e2) / 1e2;
-      risultati[i]['Percentuale'] = Math.round(risultati[i]['Kcal'] / valoriInput.totaleKcal * 100);
-      i++
-    }
- 
-    // Add qtyCarb to the valoriInput obj
-    valoriInput.qtyCarb = Math.round(risultati[2]['Grammi'] / valoriInput.peso * 1e2) / 1e2;
-    // qtyCarb in the form
-    document.getElementById('qtyCarb').value = valoriInput.qtyCarb;
-
-
-    generateTableHead(table, data);
-    generateTable(table, risultati);
-    
+const resetTable = () => {
+  if (document.querySelector("thead")) {
+    document.querySelector("thead").remove()
   }
-  console.log(valoriInput, risultati);
+}
+calcola.addEventListener('click', function () {
+  input.forEach(e => valoriInput[e.id] = Math.round(e.value * 1e2) / 1e2);
+  resetTable();
+  calcolo();
+  generateTableHead(table, data);
+  generateTable(table, risultati);
 });
-
-
